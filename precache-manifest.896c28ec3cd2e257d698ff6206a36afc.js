6c1574e671136b64b398f5fe0b7ffda81911d79b
molepoka <molepoka@webmail.co.za>
2020-06-08 20:09:03 +0200

    Updates

---
+++ precache-manifest.896c28ec3cd2e257d698ff6206a36afc.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1fb97ccb8d0b4ee3185e6b48e6c0727",
+    "url": "/ps4Request2/index.html"
+  },
+  {
+    "revision": "9b7aa2aa36a67902f94d",
+    "url": "/ps4Request2/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "1bdc8a289a7a4c32fe42",
+    "url": "/ps4Request2/static/js/2.9122b92a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ps4Request2/static/js/2.9122b92a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b7aa2aa36a67902f94d",
+    "url": "/ps4Request2/static/js/main.bc04901e.chunk.js"
+  },
+  {
+    "revision": "2fc0ec861f164e0c58a7",
+    "url": "/ps4Request2/static/js/runtime-main.e732e5f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ps4Request2/static/media/logo.5d5d9eef.svg"
+  }
+]);
